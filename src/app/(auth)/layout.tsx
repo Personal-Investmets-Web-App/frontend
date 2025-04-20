31a09e6053110055ecdf696b2351dd5a5ca1e386
San Valen <osoriovalentin5@gmail.com>
2025-04-20 16:42:47 -0500

    add logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { CashCatLogo } from "@/core/app/components/icons/cashcat-logo";
 import { ModeToggle } from "@/core/app/components/theme-toggle";
 import { Metadata } from "next";
 
@@ -13,7 +14,14 @@ export default function AuthLayout({
 }>) {
   return (
     <main className="flex items-center h-screen w-screen">
-      <div className="hidden xl:block md:w-1/3 bg-primary h-full"></div>
+      <div className="hidden xl:block md:w-1/3 bg-primary h-full">
+        <div className="flex items-center justify-center h-full">
+          <div className="w-1/2 text-background">
+            <CashCatLogo />
+            <h1 className="text-4xl font-bold mt-4 text-center">Welcome to CashCat</h1>
+          </div>
+        </div>
+      </div>
       <div className="w-full xl:w-2/3 h-full flex flex-col items-center justify-between">
         <div className="w-full flex justify-end items-center p-4">
           <ModeToggle />
